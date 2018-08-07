7bf1f0fdb95d69407a3a642915b7d94f4de17464
arc <divyanshu.agrawal@e-arc.com>
2018-08-07 19:39:13 +0530

    launch.json

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-var http = require('http')
+
+var http = require('http');
+var employeeService = require('./lib/employees');
+var responder = require('./lib/resGenerator');
+var staticFile = responder.staticFile('/public');
+
 
 http.createServer(function (req, res) {
     // A parsed url to work with in case there are parameters
     var _url;
     // In case the client uses lower case for methods.
+
     req.method = req.method.toUpperCase();
+
     console.log(req.method + ' ' + req.url);
     if (req.method !== 'GET') {
-    res.writeHead(501, {
-    'Content-Type': 'text/plain'
-    });
-    return res.end(req.method + ' is not implemented by this➥server.');
+        res.writeHead(501, {
+            'Content-Type': 'text/plain'
+        });
+        return res.end(req.method + ' is not implemented by this➥server.');
     }
     if (_url = /^\/employees$/i.exec(req.url)) {
-    // return a list of employees
-    res.writeHead(200);
-    return res.end('employee list');
+        employeeService.getEmployees(function (error, data) {
+            if (error) {
+                return responder.send500(error, res);
+            }
+            return responder.sendJson(data, res);
+        });
     } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
-    // find the employee by the id in the route
-    res.writeHead(200);
-    return res.end('a single employee');
-    } else {
-    // try to send the static file
-    res.writeHead(200);
-    res.end('static file maybe');
+        employeeService.getEmployee(_url[1], function (error, data) {
+            if (error) {
+                return responder.send500(error, res);
+            }
+            if (!data) {
+                return responder.send404(res);
+            }
+            return responder.sendJson(data, res);
+        });
     }
-    }).listen(1337, '127.0.0.1');
+    else {
+        res.writeHead(200);
+        res.end('static file maybe');
+    }
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
